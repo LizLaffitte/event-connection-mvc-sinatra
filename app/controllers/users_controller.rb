a5dd8ee1dfe5e942dc8dfcea4db4466fd3a1e3b5
laffitte.digital@gmail.com <laffitte.digital@gmail.com>
2020-02-04 20:33:11 -0500

    Created framework for controller actions/defining routes for users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+
+    #display form to create new user
+    get '/signup' do
+        erb :'users/signup'
+    end
+
+    #create new user
+    post '/signup' do
+    end
+
+    #display form to login
+    get '/login' do
+        erb :'/users/login'
+    end
+
+    #does user exist? | does login info match what's in the table  | set session id/log in user
+    post '/login' do
+    end
+
+    #read user | profile | See events that belong to user
+    get '/user/:id' do
+    end
+
+    #clear session id/log user out
+    get '/logout' do
+    end
+
 end

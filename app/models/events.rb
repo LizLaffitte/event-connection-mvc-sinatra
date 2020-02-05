7d4d420339678cfc2c20644efc147f6d55a91ae3
laffitte.digital@gmail.com <laffitte.digital@gmail.com>
2020-02-04 19:58:57 -0500

    Created Events model and created events table migration.

---
+++ app/models/events.rb
@@ -0,0 +1,7 @@
+class Events < ActiveRecord::Base
+    belongs_to :user
+    validates :name, presence: true
+    validates :date, presence: true
+    validates :location, presence: true
+    validates :details, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_05_005013) do
+ActiveRecord::Schema.define(version: 2020_02_05_005338) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.date "date"
+    t.string "location"
+    t.string "details"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"

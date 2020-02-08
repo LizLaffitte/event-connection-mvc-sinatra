2bee6d40547932ab8c2c03615866dc640b32b651
laffitte.digital@gmail.com <laffitte.digital@gmail.com>
2020-02-08 14:37:02 -0500

    Fixed /events/:id view

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,11 +3,10 @@ class Event < ActiveRecord::Base
     validates :name, presence: true
     validates :start_datetime, presence: true
     validates :location, presence: true
-    validates :details, presence: true
 
     def format_date(datetime_string)
         @date_obj = Date.strptime(datetime_string, '%Y-%m-%dT%H:%M')
-        @date_obj.strftime('%A')    
+        @date_obj.strftime('%A, %B %-d, %Y, %l:%M %P')    
     end
 
 

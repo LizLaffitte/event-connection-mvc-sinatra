0501fac394d82b9e983aca9eba35cf3a24f30de1
laffitte.digital@gmail.com <laffitte.digital@gmail.com>
2020-02-08 15:39:53 -0500

    Finished events edit and delete routes.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -44,16 +44,36 @@ class EventsController < ApplicationController
 
     #displays update/edit form
     get '/events/:id/edit' do
-        erb :'/events/edit'
+        @event = Event.find_by_id(params[:id])
+        if logged_in? && @event.user_id == current_user.id
+            @current_datetime = DateTime.now.strftime('%Y-%m-%dT%H:%M')
+            erb :'/events/edit'
+        else
+            redirect '/login'
+        end
     end
 
     #edits event
-    post '/events/:id' do
-        
+    patch '/events/:id' do
+        @event = Event.find_by_id(params[:id])
+        @event.update(:name => params[:name], :start_datetime => params[:start_datetime], :end_datetime => params[:end_datetime], :location => params[:location], :details => params[:details])
+        redirect "/events/#{@event.id}"
+
     end
 
     #deletes event | validate that it belongs to user trying to delete
     delete '/events/:id' do
+        event = Event.find_by_id(params[:id])
+        if logged_in?
+            if event.user_id == current_user.id 
+                event.delete
+                redirect "/user/#{current_user.id}"
+            else
+                redirect "/events/#{event.id}"
+            end
+        else
+            redirect '/login'
+        end
     end
 
 end

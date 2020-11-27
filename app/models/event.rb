32777aba67197588bc2f1a922a77e9543518fb30
lizlaffitte <laffitte.digital@gmail.com>
2020-11-27 12:43:35 -0500

    First authenticate eventbrite API access and then request events based on org ID.

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,6 +4,27 @@ class Event < ActiveRecord::Base
     validates :name, presence: true, length: { minimum: 3 }
     validates :start_datetime, presence: true
     validates :location, presence: true, length: { in: 3..50 }
+    
+    def self.eventbrite_auth
+        endpoint = 'https://www.eventbriteapi.com/v3/users/me/?token=' + ENV['API_KEY']
+        uri = URI.parse(URI.encode(endpoint))
+        api_response = Net::HTTP.get(uri)
+        response = JSON.parse(api_response)
+        if response.key?("emails")
+            self.eventbrite_techevents
+        else
+            puts "Something went wrong"
+        end
+    end
+
+    def self.eventbrite_techevents
+        flatiron = '17393370241'
+        endpoint = 'https://www.eventbriteapi.com/v3/organizations/11407647443/events/?token=' + ENV['API_KEY']
+        uri = URI.parse(URI.encode(endpoint))
+        api_response = Net::HTTP.get(uri)
+        response = JSON.parse(api_response)
+        puts response
+    end
 
     def format_date_long(datetime_string)
         @date_obj = DateTime.strptime(datetime_string, '%Y-%m-%dT%H:%M')
